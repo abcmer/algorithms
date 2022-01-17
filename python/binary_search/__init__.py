0937d5a0de92bdfa3ee7c9fcd664bf6815c65aad
Adam Siemer <14990007+abcmer@users.noreply.github.com>
2022-01-16 20:30:00 -0600

    add recurrsive binary search

--- python/binary_search/__init__.py
+++ python/binary_search/__init__.py
@@ -1,6 +1,3 @@
-from urllib.request import HTTPDigestAuthHandler
-
-
 def binary_search_int(sorted_list, value_to_find):   
     # Edge cases 
     if not sorted_list:
@@ -18,6 +15,23 @@ def binary_search_int(sorted_list, value_to_find):
             low = mid + 1
     return False
 
+def binary_search_recurrsive(sorted_list, value_to_find):
+    return helper(sorted_list, value_to_find, 0, len(sorted_list)-1)
+
+def helper(sorted_list, value_to_find, left, right):
+    # Return false if left pointer becomes greater than right
+    if left > right:
+        return False
+    # Init mid
+    mid = (right - left) // 2 + left
+    # Check if mid value equals value_to_find
+    # Else recurrively call helper function again
+    if sorted_list[mid] == value_to_find:
+        return True
+    elif value_to_find < sorted_list[mid]:
+        return helper(sorted_list, value_to_find, left, mid - 1)
+    else:
+        return helper(sorted_list, value_to_find, mid + 1, right)
 
 
 
--- python/tests/test_binary_search.py
+++ python/tests/test_binary_search.py
@@ -1,8 +1,29 @@
-from binary_search import binary_search_int
+from binary_search import binary_search_int, binary_search_recurrsive
+
 def test_binary_search_ints():
     assert binary_search_int([1,2,3,4,5,6,7,8,9,10], 7) == True
+    assert binary_search_int([1,2,3,4,5,6,7,8,9], 7) == True
+    assert binary_search_int([1,3], 2) == False
+    assert binary_search_int([1,3], 3) == True
+    assert binary_search_int([1,3], 1) == True
+    assert binary_search_int([1], 2) == False
+    assert binary_search_int([1], 1) == True
     assert binary_search_int([1,2,3], 2) == True 
     assert binary_search_int([1,2,3], 4) == False
     assert binary_search_int([1,2,3,4,5,6,7,8,9,10], 11) == False
     assert binary_search_int([1,2,3,4,5,6,7,8,9,10], -2) == False
     assert binary_search_int([], 5) == False
+
+def test_binary_search_recurrsive():
+    assert binary_search_recurrsive([1,2,3,4,5,6,7,8,9,10], 7) == True
+    assert binary_search_recurrsive([1,2,3,4,5,6,7,8,9], 7) == True
+    assert binary_search_recurrsive([1,3], 2) == False
+    assert binary_search_recurrsive([1,3], 3) == True
+    assert binary_search_recurrsive([1,3], 1) == True
+    assert binary_search_recurrsive([1], 2) == False
+    assert binary_search_recurrsive([1], 1) == True
+    assert binary_search_recurrsive([1,2,3], 2) == True 
+    assert binary_search_recurrsive([1,2,3], 4) == False
+    assert binary_search_recurrsive([1,2,3,4,5,6,7,8,9,10], 11) == False
+    assert binary_search_recurrsive([1,2,3,4,5,6,7,8,9,10], -2) == False
+    assert binary_search_recurrsive([], 5) == False    
